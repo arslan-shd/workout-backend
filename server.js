4c232e1aef20979f6328c0ec487cc9993062ec55
Arsla-1 <105283537+Arsla-1@users.noreply.github.com>
2023-12-12 17:22:23 +0530

    first-commit

---
+++ server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+const cors = require("cors");
+
+// express app
+const app = express();
+
+// middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/workouts", workoutRoutes);
+
+// connect to db
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port 4000!!");
+    });
+  })
+  .catch((err) => console.log(err));
